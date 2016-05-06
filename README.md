4f9509ce7b91eaa311cef7c19aebf4b90f640a6d
Abhinav Sinha <abhinav.geek@gmail.com>
2016-05-05 17:16:53 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Personal project to become familiar with android bluetooth API
+
+### Current Objective
+
+1. Display list of bluetooth devices
+  a. Maybe filter to only Dotti devices
+2. Button to make a random pixel a random color
---
+++ app/src/main/java/io/github/epukaza/dotticontrol/DottiUpdate.java
@@ -0,0 +1,13 @@
+package io.github.epukaza.dotticontrol;
+
+import android.os.AsyncTask;
+
+/**
+ * Created by abhinav on 5/5/16.
+ */
+public class DottiUpdate extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
